e7690f4dca32653e2cdf7338ec42f8506eb3d35f
Sulivan Werle <91008337+SulivanW@users.noreply.github.com>
2021-09-19 19:11:35 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,17 @@
-const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
+const { auteur, bot } = require("../config.json")
+
 module.exports.run = async (client, message, args) => {
     var ping = Math.round(client.ws.ping)
-    const newEmbed = new MessageEmbed()
+    const PingEmbed = new MessageEmbed()
         .setColor('#e38f0e')
         .setTitle('Commande Ping !')
+        .setDescription(`Commande help de ${bot}`)
         .addFields(
             { name: 'Ping trouver !', value: `${ping}` },
         )
-        .setTimestamp()
-        message.channel.send({ embeds: [newEmbed] });
+        .setFooter(`${auteur}`)
+        message.channel.send({ embeds: [PingEmbed] });
     console.log(`🏓 Commande Ping ! \nDans le serveur : ${message.guild.name}`)
 
 }

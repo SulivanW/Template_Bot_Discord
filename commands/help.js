96dfe47065b9c045a1304e366c56e1136db8d044
Sulivan Werle <91008337+SulivanW@users.noreply.github.com>
2021-09-19 19:04:42 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,14 @@
 const { MessageEmbed } = require("discord.js");
-const Discord = require("discord.js")
-const auteur = require("../config.json")
-const bot = require("../config.json")
+const { auteur, bot } = require("../config.json")
 
 module.exports.run = (client, message, args) => {
     const embed = new MessageEmbed()
-    .setDescription(`Commande help de ${bot.bot}`)
+    .setDescription(`Commande help de ${bot}`)
     .addField("`Admin Commandes : `", "`En cours de dev...`")
     .addField("`Modération Commandes : `", "`En cours de dev...`")
     .addField("`Misc Commandes : `", "`/ping` | `En cours de dev...`")
     .addField("`Musique Commandes : `", "`En cours de dev...`")
-    .setFooter(`${auteur.auteur}`)
+    .setFooter(`${auteur}`)
 
     message.channel.send({embeds: [embed]})
 }

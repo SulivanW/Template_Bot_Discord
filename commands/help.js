c9bfcbd5c975c32857048c3e93d167e87780e684
Sulivan Werle <91008337+SulivanW@users.noreply.github.com>
2021-09-19 19:14:40 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,11 +6,11 @@ module.exports.run = (client, message, args) => {
     .setDescription(`Commande help de ${bot}`)
     .addField("`Admin Commandes : `", "`En cours de dev...`")
     .addField("`Modération Commandes : `", "`En cours de dev...`")
-    .addField("`Misc Commandes : `", "`/ping` | `En cours de dev...`")
+    .addField("`Misc Commandes : `", "`/help` | `/ping` | `En cours de dev...`")
     .addField("`Musique Commandes : `", "`En cours de dev...`")
     .setFooter(`${auteur}`)
-
     message.channel.send({embeds: [embed]})
+    console.log(`🙋 Commande Help ! \nDans le serveur : ${message.guild.name}`)
 }
 module.exports.config = {
     name: "help"

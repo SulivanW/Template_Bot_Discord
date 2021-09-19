7c7c243baab0bbf7c5b14875b72a0e689f15da64
Sulivan Werle <91008337+SulivanW@users.noreply.github.com>
2021-09-19 16:16:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
-const token = require('./config.json');
+const client = new Discord.Client({intents: Object.values(Discord.Intents.FLAGS)})
 const AsciiTable = require('ascii-table')
+const { prefix, token} = require('./config.json');
 
-var table = new AsciiTable('Status On/Off')
+var table = new AsciiTable('Bot is Activate')
 table
-  .setHeading('Bot : ', `${client.user.tag}`)
-  .addRow('Commande : ', 'Not Found')
-  .addRow('Event : ', 'Not Found')
-  .addRow('Api : ', 'Online')
+  .setBorder('*')
+  .setHeading('Command ', 'and', 'Event')
+  .addRow('active', 'in', 'activate')
  
 console.log(table.toString())
 
@@ -16,4 +16,4 @@ client.on('ready', () => {
     client.user.setStatus('dnd'); //dnd(ne pas déranger), invisible(Déconnecter), online(Activer vert), idle(Absent)
 });
 
-client.login(token)
+client.login(token);
